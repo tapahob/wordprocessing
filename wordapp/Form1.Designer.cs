e51e16f69b7340aeca317ef03930ecf8da0bf78b
tapahob <lukyanchenkoam@gmail.com>
2017-05-11 23:01:43 +0300

    initialCommit

---
+++ wordapp/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace wordapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.KillVictimsCB = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(364, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // KillVictimsCB
+            // 
+            this.KillVictimsCB.AutoSize = true;
+            this.KillVictimsCB.Location = new System.Drawing.Point(364, 120);
+            this.KillVictimsCB.Name = "KillVictimsCB";
+            this.KillVictimsCB.Size = new System.Drawing.Size(15, 14);
+            this.KillVictimsCB.TabIndex = 2;
+            this.KillVictimsCB.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(364, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "txt";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(32, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(415, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Обработка текста (убираем лишние слова)";
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.fileLabel.Location = new System.Drawing.Point(480, 58);
+            this.fileLabel.MaximumSize = new System.Drawing.Size(500, 21);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(118, 21);
+            this.fileLabel.TabIndex = 5;
+            this.fileLabel.Text = "No file chosen";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(33, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(298, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Выберите файл на локальном диске";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(33, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(253, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Удалить слова длинной менее:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(33, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(232, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Удалить знаки препинания?";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(364, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Обработать и отослать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.Filter = "\"Text files\"|*.txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1007, 588);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.KillVictimsCB);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Word Processing";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox KillVictimsCB;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ wordapp/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace wordapp
+{
+    public partial class Form1 : Form
+    {
+        String text = String.Empty;
+        String result = String.Empty;
+        int maxWordLength = 1;
+
+        string[] safeSigns = { "!", "–", "«", "»", "_", "?", "(", ")", ";", "*", "/" };
+        string[] victims = { ".", ":", ",", "\"", "'"};
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {                        
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                fileLabel.Text = "No file chosen";
+                return;
+            }
+            var fileLength = new FileInfo(openFileDialog1.FileName).Length / 1024 / 1024;
+            if (fileLength > 5)
+            {
+                MessageBox.Show("HTTP ERROR 500", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            using (StreamReader sr = new StreamReader(openFileDialog1.FileName, Encoding.Default))
+            {
+                text = sr.ReadToEnd();
+            }
+                
+            fileLabel.Text = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            var path = saveFileDialog1.FileName;
+            processText();
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, Encoding.Default))
+                {
+                    sw.Write(result);
+                }
+            } catch (Exception)
+            {
+                MessageBox.Show("Не удалось сохранить файл.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            return;            
+        }
+
+        private void processText()
+        {
+            if (text == String.Empty)
+                return;
+
+            text = text.Replace(Environment.NewLine, "");
+            if (KillVictimsCB.Checked)
+            {
+                foreach (var symbol in victims)
+                {
+                    text = text.Replace(symbol, "");
+                }
+                
+            }
+
+            var split = text.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i=0; i<split.Length; ++i)
+            {
+                if (getTrueWordLength(split[i]) <= maxWordLength)
+                {
+                    split[i] = "";
+                }
+            }
+
+            result = String.Join(" ", split.Where(x => !x.Equals("")));
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string newText = ((TextBox)sender).Text;
+            int newSize = 0;
+            if (int.TryParse(newText, out newSize))
+            {
+                maxWordLength = newSize < 0 ? 0 : newSize;
+            }
+            else maxWordLength = 0;
+        }
+
+        private int getTrueWordLength(string word)
+        {
+            int initialLength = word.Length;
+            foreach(var item in victims)
+            {
+                word = word.Replace(item, "");
+            }
+            int newLength = word.Length;
+            return newLength == 0 ? 1 : newLength;
+        }
+    }
+}
